1f801bae24fb41eec7f7b38edfcf650b8026fe87
zubirimanteo irakaslea <zubirimanteo.irakaslea@gmail.com>
2014-10-01 20:01:54 +0200

    chat

--- 10-socket-io/app.js
+++ 10-socket-io/app.js
@@ -1,26 +1,21 @@
 // http://socket.io/
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var fs = require('fs');
+// https://github.com/guille/chat-example.git
 
-app.listen(3000);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
 
-    res.writeHead(200);
-    res.end(data);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log(msg);
+    io.emit('chat message', msg);
   });
-}
+});
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
