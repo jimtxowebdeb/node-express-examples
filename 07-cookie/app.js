b79598d73b6498911f4f9ffca55f47629f64742c
irakasleibiltaria <irakasleibiltaria@gmail.com>
2014-12-16 22:43:33 +0100

    cookie

--- 07-cookie/app.js
+++ 07-cookie/app.js
@@ -9,24 +9,24 @@ app.use(cookieParser());
 
 // http://localhost:3000/setcookie
 app.get('/setcookie', function(req, res) {
-    
+
     //res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
-    
+
     // The maxAge option is a convenience option for setting "expires" relative to the current time in milliseconds. The following is equivalent to the previous example.
     res.cookie('rememberme', '1', { maxAge: 900000, httpOnly: true });
 
     // An object may be passed which is then serialized as JSON, which is automatically parsed by the bodyParser() middleware.
     res.cookie('cart', { items: [1,2,3] });
     res.cookie('cart', { items: [1,2,3] }, { maxAge: 900000 });
-    
+
     res.send('Cookie created');
 });
 
 // http://localhost:3000/readcookie
 app.get('/readcookie', function(req, res) {
     var rememberme = "rememberme:" + req.cookies.rememberme;
-    var cart = "cart:" + req.cookies.cart;
-    
+    var cart = "cart:" + JSON.stringify(req.cookies.cart);
+
     res.send(rememberme + "<br />" + cart.toString());
 });
 
