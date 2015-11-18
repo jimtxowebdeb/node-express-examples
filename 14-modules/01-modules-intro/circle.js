81fe3e3646d4ff6469792a2264b395ac49a40f25
zmwebdev <zmwebdev@gmail.com>
2015-11-18 07:19:16 +0000

    modules

---
+++ 14-modules/01-modules-intro/circle.js
@@ -0,0 +1,14 @@
+// https://nodejs.org/api/modules.html
+
+// Variables local to the module will be private
+// Variable PI is private
+
+var PI = Math.PI;
+
+exports.area = function (r) {
+    return PI * r * r;
+}
+
+exports.circumference = function (r) {
+    return 2 * PI * r;
+}
