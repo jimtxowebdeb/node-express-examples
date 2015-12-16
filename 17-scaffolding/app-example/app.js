2e1a67280bbfc2a2038c38a373a4f2814ed5f5d6
zmwebdev <zmwebdev@gmail.com>
2015-12-16 11:53:13 +0000

    m

--- 17-scaffolding/app-example/app.js
+++ 17-scaffolding/app-example/app.js
@@ -1,13 +1,13 @@
 var express = require('express')
   , app = express()
 
-var db = require('models/db')
+var db = require('./models/db')
 
-app.engine('jade', require('jade').__express)
-app.set('view engine', 'jade')
+app.engine('handlebars', require('express-handlebars'));
+app.set('view engine', 'handlebars');
 
-app.use('/comments', require('controllers/comments'))
-app.use('/users', require('controllers/users'))
+app.use('/comments', require('./controllers/comments'))
+// app.use('/users', require('./controllers/users'))
 
 // Connect to Mongo on start
 db.connect('mongodb://localhost:27017/mydatabase', function(err) {
@@ -15,8 +15,17 @@ db.connect('mongodb://localhost:27017/mydatabase', function(err) {
     console.log('Unable to connect to Mongo.')
     process.exit(1)
   } else {
-    app.listen(3000, function() {
+/*    app.listen(3000, function() {
       console.log('Listening on port 3000...')
     })
+*/  
+    
+    var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+    var ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1";
+
+    var server = app.listen(port, ip, function(){
+      console.log('Listening in port %d', server.address().port);
+    });
+    
   }
 })
--- 17-scaffolding/app-example/controllers/comments.js
+++ 17-scaffolding/app-example/controllers/comments.js
@@ -5,12 +5,15 @@ var Comments = require('../models/comments')
 
 router.get('/all', function(req, res) {
   Comments.all(function(err, docs) {
+    //if (err) throw err;
     res.render('comments', {comments: docs})
+    // res.send("Hello")
   })
 })
 
 router.get('/recent', function(req, res) {
   Comments.recent(function(err, docs) {
+    // if (err) throw err;
     res.render('comments', {comments: docs})
   })
 })
