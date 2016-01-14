aa04d63e54f0c8792829085bd202cf8c6ba2fe0a
zmwebdev <zmwebdev@gmail.com>
2016-01-14 10:18:12 +0000

    upload

---
+++ 02-form/01-form-uploadfile/app.js
@@ -0,0 +1,46 @@
+/*
+http://expressjs.com/en/api.html
+
+In Express 4, req.files is no longer available on the req object by default. 
+To access uploaded files on the req.files object, use a multipart-handling middleware 
+like busboy, multer, formidable, multiparty, connect-multiparty, or pez.
+*/
+var express = require('express');
+var app = express();
+
+// body-parser for POST
+// https://github.com/expressjs/body-parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// public files
+app.use(express.static(__dirname + '/public'));
+
+var multer  = require('multer')
+var upload = multer({dest: __dirname + '/public/uploads/'})
+
+// **********************************************
+
+app.get('/', function (req, res) {
+  //res.sendFile(__dirname + '/public/hello.html');
+  // http://expressjs.com/4x/api.html#res.redirect
+  res.redirect('hello.html');
+});
+
+// POST /user/
+
+app.post('/upload', upload.single('image'), function (req, res) {
+    // req.file is the `avatar` file 
+    // req.body will hold the text fields, if there were any 
+    console.log("POST /user");
+    res.send('File uploaded');
+});
+
+
+// ************************************************
+
+
+var server = app.listen(process.env.PORT || 3000, function(){
+    console.log('Listening in port %d', server.address().port);
+});
