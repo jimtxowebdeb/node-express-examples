4cb5aceae8e474dbb80a7d441db345797d9233f5
zmwebdev <zmwebdev@gmail.com>
2016-01-14 08:30:12 +0000

    express router post

--- 15-express-generator/myapp/routes/users.js
+++ 15-express-generator/myapp/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* POST users listing. */
+router.post('/', function(req, res, next) {
+  res.send('User added: ' + req.body.user);
+});
+
 module.exports = router;
