6749d4e0f8ee6839477b5f3c6a7eb53fed755910
zmwebdev <zmwebdev@gmail.com>
2016-02-12 09:53:53 +0000

    socket.io

--- 10-socket-io/app.js
+++ 10-socket-io/app.js
@@ -1,9 +1,20 @@
-// http://socket.io/
+  // http://socket.io/
 // https://github.com/guille/chat-example.git
 
+/*
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+*/
+
+// express
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// socket.io
+var io = require('socket.io')(http);
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
