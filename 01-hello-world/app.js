87ed479c6b8899431f2917f2defc142ab47a631b
zmwebdev <zmwebdev@gmail.com>
2016-11-30 08:54:07 +0000

    facebook

--- 01-hello-world/app.js
+++ 01-hello-world/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('Kaixo');
+	res.send('Kaixo2');
 });
 
 // http://localhost:3000/hello.txt?q=xxxxxx
---
+++ 09-API/10-facebook/Readme.md
@@ -0,0 +1,3 @@
+https://www.npmjs.com/package/fb
+
+https://developers.facebook.com/docs/graph-api/reference/v2.8/object/likes
